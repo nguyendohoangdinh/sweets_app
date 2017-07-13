4eda0999e8d678969bd43aa0c63cb09e8b63385b
Hoang Dinh <nguyendohoangdinh@gmail.com>
2017-07-13 17:09:54 +0700

    95%

---
+++ js/jquery.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+// 	$("#datetimepicker").on('click', function(){
+// 		alert(1);
+// 	});
+// });
+
+   //  $("#datetimepicker").datetimepicker({
+   //      format: "dd MM yyyy - hh:ii"
+   //  });
+   // 
+   // $(function () {
+   // 		$('#datetimepicker1').datetimepicker();
+   //  });
+   $(function () {
+        $('#datetimepicker1').datetimepicker();
+    });
